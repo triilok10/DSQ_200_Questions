6b39b1f58409a83ed3ff47123dd1397073b0ce93
triilok10 <ittiku3@gmail.com>
2026-04-05 14:37:33 +0530

    Add project files.

---
+++ DSQ 200 Questions/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+
+
+//Name Space
+namespace TwoHundredQuestions
+{
+    // Class 
+    public class Program
+    {
+
+        // Main Function
+        public static void Main(string[] args)
+        {
+
+            Program obj = new Program();
+            obj.SumOfArray([1, 2, 3, 4, 1, 2, 3, 4, 1, 3]);
+
+
+
+        }
+
+
+        //Find the sum of all elements in an array.
+        public int SumOfArray(int[] n)
+        {
+            int sum = 0;
+            return sum;
+        }
+
+
+
+
+
+
+
+
+    }
+}
