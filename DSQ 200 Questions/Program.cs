f9bf0ad61849c022dac8036ec9543fff717451f2
triilok10 <ittiku3@gmail.com>
2026-04-07 07:10:08 +0530

    Find the sum of all elements in an array.

--- DSQ 200 Questions/Program.cs
+++ DSQ 200 Questions/Program.cs
@@ -11,7 +11,6 @@ namespace TwoHundredQuestions
         // Main Function
         public static void Main(string[] args)
         {
-
             Program obj = new Program();
             obj.SumOfArray([1, 2, 3, 4, 1, 2, 3, 4, 1, 3]);
 
@@ -20,10 +19,15 @@ namespace TwoHundredQuestions
         }
 
 
-        //Find the sum of all elements in an array.
+        //Q1 Find the sum of all elements in an array.
         public int SumOfArray(int[] n)
         {
             int sum = 0;
+            foreach(var i in n)
+            {
+                sum += i;
+            }
+            Console.WriteLine("Sum of Array",sum);
             return sum;
         }
 
